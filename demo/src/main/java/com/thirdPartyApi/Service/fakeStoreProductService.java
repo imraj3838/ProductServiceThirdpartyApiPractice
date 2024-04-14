68a1452f07ea07d8b8ccb19a7241b847d2a28396
priyanshu <imraj383foru@gmail.com>
2024-04-14 16:20:38 +0530

    Priyanshu commit

--- demo/src/main/java/com/thirdPartyApi/Service/fakeStoreProductService.java
+++ demo/src/main/java/com/thirdPartyApi/Service/fakeStoreProductService.java
@@ -1,12 +1,15 @@
 package com.thirdPartyApi.Service;
 
 import com.thirdPartyApi.models.Product;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class fakeStoreProductService implements productService {
 
 
     @Override
     public Product getSingleProduct(){
+        System.out.println("I am coming from product Service");
         return null;
     }
 }
